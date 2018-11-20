bbcb7aac0bf3ee8f46f945f1c893225a593b11d7
Matsui Yuki <lanterne.rouge.pen@gmail.com>
2018-11-20 10:20:21 +0900

    update Config to AthenaJDBC2.x
    Item name and order conformed to "Installation and Configuration Guide".
    Note: "AwsRegion" is included in "Url".

--- src/test/scala/scalikejdbc/athena/ConfigSpec.scala
+++ src/test/scala/scalikejdbc/athena/ConfigSpec.scala
@@ -7,18 +7,18 @@ class ConfigSpec extends FunSpec with OptionValues {
   describe("Config") {
     it("default db") {
       val config = new Config("default")
-      assert(config.url === "jdbc:awsathena://athena.us-east-2.amazonaws.com:443")
-      assert(config.options.getProperty("s3_staging_dir") === "s3://query-results-bucket/folder/")
-      assert(config.options.getProperty("log_path") === "logs/application.log")
+      assert(config.url === "jdbc:awsathena://AwsRegion=us-east-2")
+      assert(config.options.getProperty("S3OutputLocation") === "s3://query-results-bucket/folder/")
+      assert(config.options.getProperty("LogPath") === "logs/application.log")
       assert(config.getTmpStagingDir.isEmpty)
     }
     it("named db") {
       val config = new Config('athena)
-      assert(config.url === "jdbc:awsathena://athena.ap-southeast-1.amazonaws.com:443")
-      assert(config.options.getProperty("s3_staging_dir") !== "s3://query-results-bucket/folder")
-      assert(config.options.getProperty("s3_staging_dir").startsWith("s3://query-results-bucket/folder") === true)
-      assert(config.options.containsKey("log_path") === false)
-      assert(config.getTmpStagingDir.value === config.options.getProperty("s3_staging_dir"))
+      assert(config.url === "jdbc:awsathena://AwsRegion=ap-southeast-1")
+      assert(config.options.getProperty("S3OutputLocation") !== "s3://query-results-bucket/folder")
+      assert(config.options.getProperty("S3OutputLocation").startsWith("s3://query-results-bucket/folder") === true)
+      assert(config.options.containsKey("LogPath") === false)
+      assert(config.getTmpStagingDir.value === config.options.getProperty("S3OutputLocation"))
     }
     it("invalid settings") {
       val config = new Config('duplicated)
